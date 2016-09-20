793187b7d6184dd62d497809af79a6910d177844
John Freeman <john.freeman.opensource@gmail.com>
2016-09-21 00:25:39 +0100

    Protect against VersionEye timeouts
    Seems to be a frequent problem.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,9 +1,29 @@
-./mvnw clean install --batch-mode --show-version
+#!/bin/bash
+
+./mvnw clean install --batch-mode --show-version || exit 1
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "master" ]; then
-        ./mvnw versioneye:update --batch-mode
+        timeout --kill-after=30s 2m \
+            ./mvnw versioneye:update --batch-mode
+        exit_code=$?
+        if [ "$exit_code" == '124' ] || [ "$exit_code" == '137' ]; then
+            echo 'Timed out performing versioneye:update'
+        else
+            if [ "$exit_code" != '0' ]; then
+                exit $exit_code
+            fi
+        fi
     else
-        ./mvnw versioneye:securityAndLicenseCheck --batch-mode
+        timeout --kill-after=30s 2m \
+            ./mvnw versioneye:securityAndLicenseCheck --batch-mode
+        exit_code=$?
+        if [ "$exit_code" == '124' ] || [ "$exit_code" == '137' ]; then
+            echo 'Timed out performing versioneye:securityAndLicenseCheck'
+        else
+            if [ "$exit_code" != '0' ]; then
+                exit $exit_code
+            fi
+        fi
     fi
 fi
