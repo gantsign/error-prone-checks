a31bfc797309bacbe212c6cacbf275a6ced79482
John Freeman <john.freeman.opensource@gmail.com>
2016-09-21 15:25:29 +0100

    Added suggested fixes
    Encourage use of java.nio.charset.StandardCharsets.

--- require-charset/src/test/java/com/github/gantsign/errorprone/require/charset/testdata/NewStringWithoutCharsetPositiveCases.java
+++ require-charset/src/test/java/com/github/gantsign/errorprone/require/charset/testdata/NewStringWithoutCharsetPositiveCases.java
@@ -7,7 +7,7 @@ public class NewStringWithoutCharsetPositiveCases {
   private static final byte[] TEST_BYTES = "test".getBytes(UTF_8);
 
   public void newString() {
-    // BUG: Diagnostic contains: Charset must be specified when constructing a new String from a byte array.
+    // BUG: Diagnostic contains: java.nio.charset.Charset must be specified when constructing a new java.lang.String from a byte array.
     new String(TEST_BYTES);
   }
 
